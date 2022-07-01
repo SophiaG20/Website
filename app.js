1c57bbdb34d4346a1dc7a2c266499ab1a8a7645c
SophiaG20.x <SophiaG20@SophiaG20.com>
2022-07-01 05:03:28 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
+functions askfeelings() {
+  let askfeelings = "Yellow";
+  let otherYellow = "yellow"; 
 
+console.log(yellow == otherYellow);
+
+console.logprompt==("What is your favorite color?");
+
+let usersColor = prompt("What is your favorite Avenger?");
+console.log(usersColor);
+olor = yellow
+
+console.log(yellow);
+console.l
+
+let x = 5; 
+let y ="5"; 
+
+console.log(x,y); // number, string
+console.log(x == y); // equals
+console.log(x === y); // STRICTLY equals
+console.log(x = y); // reassign
+console.log(x);
+
+//if the user's fav color is yellow, say "Mine too!"
+// otherwise say "That's a good color"
+
+//if (usersColor == "YELLOW"){
+//    console.log("I also like YELLOW!!!");
+//} else if (usersColor.toLowerCase() == ("yellow") {
+//    console.log("Mine too!");
+//} 
+//else  {
+//    console.log("That's still a nice color.");
+// }
+
+ if (usersColor.toLowerCase()== "Iron Man"){
+    console.log("I am Iron Man");
+} else if (usersColor()== "Thor") {
+ console.log("Well he is a Pirate Angel"); 
+} else {
+    console.log("That's still a nice color.");
+}
