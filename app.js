1b066f6a37be4347603280d112c798e6b0af4be7
Sophia G20 Grzeskowiak-Amezquita <SophiaG20@SophiaG20.com>
2022-07-01 09:47:48 -0700

    this is a long time doing the same thing

--- app.js
+++ app.js
@@ -1,43 +1,38 @@
-function askfeelings() {
-  let askfeelings = "Yellow";
-  let otherYellow = "yellow"; 
+// let yellow = "Yellow";
+// let otherYellow = "yellow";
 
-console.log(yellow == otherYellow);
+// let favColor = yellow;
 
-console.logprompt==("What is your favorite color?");
+// console.log(yellow);
+// console.log(otherYellow);
 
-let usersColor = prompt("What is your favorite Avenger?");
-console.log(usersColor);
-olor = yellow
+// console.log(yellow == otherYellow);
 
-console.log(yellow);
-console.l
+let usersColor = prompt("What is your favorite color?");
+// console.log(usersColor);
 
-let x = 5; 
-let y ="5"; 
+let x = 5;
+let y = "5";
 
-console.log(x,y); // number, string
-console.log(x == y); // equals
-console.log(x === y); // STRICTLY equals
-console.log(x = y); // reassign
-console.log(x);
+// console.log(x, y); // number, string
+// console.log(x == y); // equals
+// console.log(x === y); // STRICTLY equals
 
-//if the user's fav color is yellow, say "Mine too!"
+// If the user's fav color is yellow, say "Mine too!"
 // otherwise say "That's a good color"
 
-//if (usersColor == "YELLOW"){
-//    console.log("I also like YELLOW!!!");
-//} else if (usersColor.toLowerCase() == ("yellow") {
-//    console.log("Mine too!");
-//} 
-//else  {
-//    console.log("That's still a nice color.");
+// if (usersColor == "YELLOW"){
+//     console.log("I also like YELLOW!!!!");
+// } else if (usersColor.toLowerCase() == "yellow") {
+//     console.log("Mine too!");
+// } else {
+//     console.log("That's still a nice color.");
 // }
 
- if (usersColor.toLowerCase()== "Iron Man"){
-    console.log("I am Iron Man");
-} else if (usersColor()== "Thor") {
- console.log("Well he is a Pirate Angel"); 
+if (usersColor == "YELLOW"){
+    document.write("I also like YELLOW!!!!");
+} else if (usersColor.toLowerCase() == "yellow") {
+    document.write("Mine too!");
 } else {
-  console.log("That's still a nice color.");
+    document.write("That's still a nice color.");
 }
