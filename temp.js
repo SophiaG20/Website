9902b81b74c2abee6d8ee209617cf039779dbaa9
Sophia G20 Grzeskowiak-Amezquita <SophiaG20@SophiaG20.com>
2022-07-10 11:23:18 -0700

    changed the script so one temp.js works

--- temp.js
+++ temp.js
@@ -1,4 +1,5 @@
-let WhatTemp = prompt("What temp is it where you are dancing");
+let whatTemp = prompt("What temp is it where you are dancing");
+console.log(whatTemp);
 // The best temp to dance is 18 degrees to 24 degrees Celsius
 // that temp is 64.4 - 75.2 Fahrenheit
 
@@ -6,11 +7,21 @@ let WhatTemp = prompt("What temp is it where you are dancing");
 // to tell the user to turn up the heat or make it colder before dancing
 let message;
 
-if (WhatTemp <=64){
-    alert = "Warm up your dancing space";
-} else if (WhatTemp<=75){
-    alert = "Let us get ready to boogie";
-} else if (WhatTemp=>75){
-    alert = "We need to cool down to get ready to dance";
-}
+if (whatTemp <=64){
+    message = "Warm up your dancing space";
+} else if (whatTemp<=75){
+    message = "Let us get ready to boogie";
+} else if (whatTemp=>75){
+    message = "We need to cool down to get ready to dance";
+};
+
+
+// learning about functions
+// function/ name/ parameters /codeblock
+// function whatTemp(){}
+
+// function whatTemp(){
+        //code the function executes
+
+//}
   
