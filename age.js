a778d0575ef103666e4539c33fb38bc1424e8518
Sophia G20 Grzeskowiak-Amezquita <SophiaG20@SophiaG20.com>
2022-07-09 19:48:22 -0700

    second try

--- age.js
+++ age.js
@@ -1,6 +1,6 @@
 let ageStr = prompt('how old are you (I will not tell anyone)?')
 let age = Number(ageStr);
-
+// let us see if this works now 09July2022 7:47PM
 let feedback = age >= 13 ?
     'You are eligible to use these YouTube videos':
     'You must be at least 13 years old to use these YouTube videos';
