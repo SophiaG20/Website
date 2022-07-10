28c3d4f7ab923c16247a523d0a1ba972f0c2634d
Sophia G20 Grzeskowiak-Amezquita <SophiaG20@SophiaG20.com>
2022-07-10 13:49:33 -0700

    Changed code into a function and commented out the old code

--- temp.js
+++ temp.js
@@ -7,13 +7,13 @@ console.log(whatTemp);
 // to tell the user to turn up the heat or make it colder before dancing
 
 
-if (whatTemp <=64){
-    alert("Warm up your dancing space");
-} else if (whatTemp<=75){
-    alert("Let us get ready to boogie");
-} else if (whatTemp>=76){
-    alert("We need to cool down to get ready to dance");
-
+//if (whatTemp <=64){
+//   alert("Warm up your dancing space");
+// } else if (whatTemp<=75){
+//     alert("Let us get ready to boogie");
+// } else if (whatTemp>=76){
+//     alert("We need to cool down to get ready to dance");
+// }
 
 // learning about functions
 // function/ name/ parameters /codeblock
@@ -21,4 +21,13 @@ if (whatTemp <=64){
 
 // function whatTemp(){
         //code the function executes
+
+function getTempString(temperature){
+    if (temperature <=64){
+        return "Warm up your dancing space";
+    } else if (temperature<=75){
+        return "Let us get ready to boogie";
+    } else if (temperature>=76){
+          return "We need to cool down to get ready to dance";
+    }
 }
